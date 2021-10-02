1d26ee7b0ecade484f69c43bc6f69858d94f50f8
SamuelCasuyD <sammy.kazzu@outlook.com>
2021-10-02 15:15:04 -0600

    Modificacion Universidad bloque de codigo informativo

--- src/App.js
+++ src/App.js
@@ -15,6 +15,26 @@ class App extends Component {
 
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        
+        <br/>
+        
+        <h2>
+        Sistemas Operativos 1
+        </h2>
+        <p>
+        	El curso de Sistemas Operativos 1 tiene como objetivo principal que el estudiante aplique 
+y entienda el concepto de Exclusión mutua entre procesos. Y pueda implementar las 
+diferentes soluciones que existen en la administración de Sistemas Operativos. 
+        </p>
+        
+        <br/>
+        
+                <h2>
+        Centro Universitario
+        </h2>
+                <p>
+La Universidad Mariano Gálvez de Guatemala es una universidad privada en Guatemala. Su nombre hace referencia al prócer y preclaro jurisconsulto Doctor José Mariano Gálvez (Jefe del Estado de Guatemala 1831-1838), fundador de la Academia de Estudios y reformador de la educación guatemalteca, quien promovió importantes innovaciones en todos los órdenes de la vida del Estado. Luchó por que la enseñanza fuera laica, fue fundador de la Biblioteca y Museo Nacional, respetó las leyes y garantías individuales, libertad de prensa y emisión del pensamiento.  
+        </p>
       </div>
     );
   }
